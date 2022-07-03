4677a96c7cea81606e4dc70c373b92813f4f3649
Beka Cru <bekacru@gmail.com>
2022-07-04 01:08:24 +0300

    search implementaition trial

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from pydoc import doc
 from flask import Flask, request, jsonify
 from search import search, document_index
 
@@ -6,18 +5,19 @@ from search import search, document_index
 app = Flask(__name__)
 
 
-@app.route('/<q>', methods=['GET'])
-def home(q):
+@app.route('/', methods=['GET'])
+def home():
+    return "Hey"
+
+
+@ app.route('/<q>', methods=['GET'])
+def search(q):
     res = search(q)
     return jsonify(res)
 
 
-@app.route('/index', methods=['POST'])
+@ app.route('/index', methods=['POST'])
 def index():
     content = request.json
     document_index(content)
     return content[0]["title"]
-
-
-if __name__ == '__main__':
-    app.run()
